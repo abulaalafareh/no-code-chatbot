a35276dfde43162183fb51878e40ed7b42648bc7
abulaalafareh <abul.aala.fareh@gmail.com>
2025-03-15 17:22:16 +0500

    Added : loaders

--- Services/document_processing.py
+++ Services/document_processing.py
@@ -1,31 +1,24 @@
 from langchain_community.document_loaders import PyPDFLoader, CSVLoader, Docx2txtLoader, UnstructuredExcelLoader, TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-
+from parsers import recursive_character_text_splitter
 class Load_and_Chunk:
     
     def pdf_loader(file_path, chunk_size=300, chunk_overlap=20):
         loader = PyPDFLoader(file_path=file_path)
-        documents = loader.load_and_split(text_splitter=RecursiveCharacterTextSplitter(
-            separators=['\n\n'],
-            chunk_size=chunk_size,
-            chunk_overlap=chunk_overlap,
-            length_function=len,
-            is_separator_regex=False,
-        ))
+        data = loader.load()
+        documents = recursive_character_text_splitter(data, chunk_size, chunk_overlap)
+        print("len of docs",len(documents))
         for doc in documents:
             doc.metadata= { **doc.metadata, "file_name": file_path.split("/")[-1], "chunk_size" : chunk_size }
+        print("len of docs",len(documents))
             
         return documents
 
     def csv_loader(file_path, chunk_size=300, chunk_overlap=20):
         loader = UnstructuredExcelLoader(file_path=file_path)
-        documents = loader.load_and_split(text_splitter=RecursiveCharacterTextSplitter(
-            separators=['\n\n'],
-            chunk_size=chunk_size,
-            chunk_overlap=chunk_overlap,
-            length_function=len,
-            is_separator_regex=False,
-        ))
+        data = loader.load()
+        documents = recursive_character_text_splitter(data, chunk_size, chunk_overlap)
+
         for doc in documents:
             doc.metadata= { **doc.metadata, "file_name": file_path.split("/")[-1], "chunk_size" : chunk_size }
             
@@ -33,13 +26,9 @@ class Load_and_Chunk:
 
     def excel_loader(file_path, chunk_size=300, chunk_overlap=20):
         loader = CSVLoader(file_path=file_path)
-        documents = loader.load_and_split(text_splitter=RecursiveCharacterTextSplitter(
-            separators=['\n\n'],
-            chunk_size=chunk_size,
-            chunk_overlap=chunk_overlap,
-            length_function=len,
-            is_separator_regex=False,
-        ))
+        data = loader.load()
+        documents = recursive_character_text_splitter(data, chunk_size, chunk_overlap)
+
         for doc in documents:
             doc.metadata= { **doc.metadata, "file_name": file_path.split("/")[-1], "chunk_size" : chunk_size }
             
@@ -47,13 +36,9 @@ class Load_and_Chunk:
 
     def doc_and_docx_loader(file_path, chunk_size=300, chunk_overlap=20):
         loader = Docx2txtLoader(file_path=file_path)
-        documents = loader.load_and_split(text_splitter=RecursiveCharacterTextSplitter(
-            separators=['\n\n'],
-            chunk_size=chunk_size,
-            chunk_overlap=chunk_overlap,
-            length_function=len,
-            is_separator_regex=False,
-        ))
+        data = loader.load()
+        documents = recursive_character_text_splitter(data, chunk_size, chunk_overlap)
+        
         for doc in documents:
             doc.metadata= { **doc.metadata, "file_name": file_path.split("/")[-1], "chunk_size" : chunk_size }
             
@@ -61,13 +46,9 @@ class Load_and_Chunk:
 
     def txt_loader(file_path, chunk_size=300, chunk_overlap=20):
         loader = TextLoader(file_path=file_path)
-        documents = loader.load_and_split(text_splitter=RecursiveCharacterTextSplitter(
-            separators=['\n\n'],
-            chunk_size=chunk_size,
-            chunk_overlap=chunk_overlap,
-            length_function=len,
-            is_separator_regex=False,
-        ))
+        data = loader.load()
+        documents = recursive_character_text_splitter(data, chunk_size, chunk_overlap)
+        
         for doc in documents:
             doc.metadata= { **doc.metadata, "file_name": file_path.split("/")[-1], "chunk_size" : chunk_size }
             
